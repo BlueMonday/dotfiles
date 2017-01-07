e71293d1f750daca74f64821bfaff2c112d7da6e
Steven Erenst <stevenerenst@gmail.com>
2017-01-07 02:11:55 -0800

    Add gitignore
    Configure git to ignore directories created by emacs which do not need
    to be version controlled such as undo histories.
    
    Signed-off-by: Steven Erenst <stevenerenst@gmail.com>

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dockerfile-mode tabbar company-go linum-relative vi-tilde-fringe vi-tilde-fringe-mode git-gutter-fringe+ git-gutter-fringe git-gutter highlight-indentation evil-commentary indent-guide rainbow-delimiters company-quickhelp company-jedi company helm-projectile powerline-evil fill-column-indicator column-marker flycheck emacs-color-theme-solarized solarized-theme powerline nlinum-relative use-package rainbow-mode helm evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
