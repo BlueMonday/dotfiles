70d6cd5b1b5d6532e3036d8b334a3c34fe390ae6
Steven Erenst <stevenerenst@gmail.com>
2017-01-07 01:37:19 -0800

    Evil mode escape quits everything
    Also switching to the Dracula theme because it is sweeet.
    
    Also configuring emacs to start up maximized since it was annoying to
    always have to manually do that.
    
    Signed-off-by: Steven Erenst <stevenerenst@gmail.com>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -39,7 +39,10 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (global-hl-line-mode 1)
 (global-linum-mode t)
+(setq vc-follow-symlinks t)
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups/")))
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
 (use-package evil
   :ensure t
@@ -48,14 +51,30 @@
   (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
   (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
   (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-  (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right))
+  (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+  ;; esc quits
+  (defun minibuffer-keyboard-quit ()
+    "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+    (interactive)
+    (if (and delete-selection-mode transient-mark-mode mark-active)
+        (setq deactivate-mark  t)
+      (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+      (abort-recursive-edit)))
+  (define-key evil-normal-state-map [escape] 'keyboard-quit)
+  (define-key evil-visual-state-map [escape] 'keyboard-quit)
+  (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+  (global-set-key [escape] 'evil-exit-emacs-state))
 
-;; https://github.com/sellout/emacs-color-theme-solarized/pull/187
-(setq color-themes '())
-(use-package color-theme-solarized
+(use-package dracula-theme
   :ensure t
   :config
-  (load-theme 'solarized-dark t))
+  (load-theme 'dracula t))
 
 (use-package linum-relative
   :ensure t
