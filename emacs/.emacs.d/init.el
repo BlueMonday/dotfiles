0b25b1fe9a7e42691442f3f272e664496462e23d
Steven Erenst <stevenerenst@gmail.com>
2017-02-15 00:08:07 -0800

    Use nicer rofi dracula theme
    Signed-off-by: Steven Erenst <stevenerenst@gmail.com>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -102,7 +102,6 @@
   :config
   (evil-mode 1)
   (define-key evil-normal-state-map (kbd "C-p") 'counsel-projectile-find-file)
-  (define-key evil-normal-state-map (kbd "C-p s") 'counsel-projectile-switch-project)
   (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
   (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
   (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
