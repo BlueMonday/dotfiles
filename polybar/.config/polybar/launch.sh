b5f34402e6a7d289e1bee3eb4890efce03aa8346
Steven Santos Erenst <stevenerenst@gmail.com>
2018-01-31 15:08:24 -0800

    Improve polybar setup with multiple monitors
    Signed-off-by: Steven Santos Erenst <stevenerenst@gmail.com>

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar mybar &
+# Stolen from: https://github.com/jaagr/polybar/issues/763
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mybar &
+    done
+else
+    polybar --reload mybar &
+fi
 
 echo "Bar launched..."
